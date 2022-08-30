401bd367e254e7fcdcffdcb1fd82e84277874e24
Rafael Souza <souza_rafael.97@outlook.com>
2022-08-30 16:22:26 -0300

    Adiciona requisito 6: Criação do cabeçalho da tabela

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import WalletForm from '../components/WalletForm';
+import Table from '../components/Table';
 
 class Wallet extends React.Component {
   render() {
@@ -12,6 +13,9 @@ class Wallet extends React.Component {
 
         <main>
           <WalletForm />
+          <section>
+            <Table />
+          </section>
         </main>
       </>
     );
